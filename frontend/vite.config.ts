3fc57bc9be0cc1744604179c67b568c57eaf9010
Nabe1228 <exile-love777@ezweb.ne.jp>
2026-05-27 11:44:55 +0900

    feat: configure vite with react plugin and auto-opening chrome browser

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+process.env.BROWSER = 'chrome'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  }
 })
